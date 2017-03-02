26523819eca49812c140e8c7c7bd5377df88957a
arnaud <arnaud.bariatti@gmail.com>
2017-03-02 16:32:14 +0100

    modified startup to use mvc and created a basic example controller

--- ConferenceApp.WebAPI/Startup.cs
+++ ConferenceApp.WebAPI/Startup.cs
@@ -16,6 +16,8 @@ namespace ConferenceApp.WebAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,7 @@ namespace ConferenceApp.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
