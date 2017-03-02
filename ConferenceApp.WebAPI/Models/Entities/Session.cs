feb3872856bc9278319e30f7c1d28c7b6a9e0340
arnaud <arnaud.bariatti@gmail.com>
2017-03-02 17:05:19 +0100

    added session model

---
+++ ConferenceApp.WebAPI/Models/Entities/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.WebAPI.Models.Entities
+{
+    public class Session
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+    }
+}
